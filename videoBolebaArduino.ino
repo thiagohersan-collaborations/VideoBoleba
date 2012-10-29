23c26759fc909319e1de423e16355da58f7fc39d
Thiago Hersan <thiago.hersan+github@gmail.com>
2012-10-29 15:28:30 -0200

    changes from tests with actual machine and video

--- videoBolebaArduino.ino
+++ videoBolebaArduino.ino
@@ -1,23 +1,24 @@
 #define ANALOG_IN A0
-#define THRESH 25
-#define ONTIME   500
-#define ONDELAY  0
+#define THRESH 100
+#define ONTIME   555
+#define ONDELAY  1000
 
 
 // state variables
 int ledState;
-unsigned long lastOn;
+unsigned long lastOn, lastOff;
 unsigned long toTurnOn;
 
 void setup() {
   // setup
-  //Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(13,OUTPUT);
   pinMode(12,OUTPUT);
 
   // initial state
   ledState = LOW;
   lastOn = millis();
+  lastOff = millis();
   toTurnOn = -1;
 
   // pin stuff
@@ -32,9 +33,9 @@ void loop() {
   // read analog in
   int val = analogRead(ANALOG_IN);
 
-  // if off and saw a value and haven't seen other value yet
+  // if off and saw a value and haven't seen other value yet and have been off for 1 second
   //    then set up a time to turn on
-  if((ledState == LOW)&&(val > THRESH)&&(toTurnOn == -1)){
+  if((ledState == LOW)&&(val > THRESH)&&(toTurnOn == -1)&&((millis()-lastOff) > 1000)){
     toTurnOn = millis()+ONDELAY;
   }
   // if off and have seen a high value and it's time to turn on
@@ -50,12 +51,14 @@ void loop() {
     ledState = LOW;
     digitalWrite(13,ledState);
     digitalWrite(12,ledState);
+    lastOff = millis();
   }
 
   Serial.write( 0xff);
   Serial.write( (val >> 8) & 0xff);
   Serial.write( val & 0xff);
 }
+
 
 
 
